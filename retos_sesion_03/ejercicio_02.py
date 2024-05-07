f49622aaba2a7332895b50b1d6af4565229370fa
mumv1973 <mumv1973@gmail.com>
2024-05-07 11:50:30 -0400

    Dialogo de amigos.

---
+++ retos_sesion_03/ejercicio_02.py
@@ -0,0 +1,11 @@
+print("A: Hola Carlos, muy buenas tardes.  ")
+print("B: Sergio, que gusto saludarte. Buenas tardes. ")
+print("A: Cuéntame, cómo está el clima por allá? Aquí hace mucho frio.")
+
+print("B: Pues aquí está iniciando el verano y ya se comienza a sentir el calor.")
+print("A: Todavía recuerdo la temporada que pasé allá y creo que esta era la mejor época ")
+print("B: Lo malo es que ya se emitieron boletines de seguridad para cuidarnos por las olas de calor que llegarán")
+print("A: Wao, me enteré algo de eso. Espero te cuides mucho y tu familia también. ")
+print("B: Estamos en eso, ya hemos tomado algunas previsiones tanto dentro como fuera de la casa.  ")
+print("A: Me parece cómico pensar que ustedes están en verano y nosotros con nieve de 60 centímetros.  ")
+print("B: Pues así es querido amigo, no tienes idea de cuanto me gustaría estar ahí con ustedes. ")
