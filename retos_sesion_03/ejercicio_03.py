fe30a7670cecafcaca1e71391ad58bc3ec38faa4
mumv1973 <mumv1973@gmail.com>
2024-05-07 12:12:21 -0400

    Proceso de barrido de habitacion.

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,63 @@
+print(" ")
+print("Preparación:")
+print(" ")
+
+print("1. Reúne los materiales necesarios:")
+print("    * Escoba")
+print("    * Recogedor de basura o bolsa de basura")
+print("    * Paño o trapo para limpiar el polvo (opcional)")
+
+print(" ")
+print("2. Retira los muebles y objetos grandes de la habitación:")
+print("    * Mueve los muebles a un lado o fuera de la habitación para tener más espacio para barrer.")
+print("    * Recoge objetos grandes como juguetes, ropa o zapatos y colócalos en un lugar adecuado.")
+
+print(" ")
+print("Barrido:")
+print(" ")
+
+print("3. Comienza barriendo desde una esquina de la habitación:")
+print("    * Utiliza movimientos largos y uniformes con la escoba.")
+print("    * Barre en secciones pequeñas y avanza hacia la otra esquina.")
+
+print(" ")
+print("4. Presta atención a las esquinas y rincones:")
+print("    * Utiliza el borde de la escoba o el recogedor de polvo para llegar a estos lugares.")
+print("    * No olvides barrer debajo de los muebles si es posible.")
+
+print(" ")
+print("5. Barre alrededor de los objetos que no puedas mover:")
+print("    * Ten cuidado de no golpear o mover los objetos accidentalmente.")
+print("    * Utiliza la escoba en diferentes ángulos para barrer alrededor de los objetos.")
+
+print(" ")
+print("Limpieza del polvo:")
+print(" ")
+
+print("6. Si lo deseas, utiliza un trapo o paño para limpiar el polvo de las superficies:")
+print("    * Comienza por las superficies más altas y baja gradualmente.")
+print("    * Presta atención a las esquinas, bordes y objetos decorativos.")
+
+print(" ")
+print("Recogida de basura:")
+print(" ")
+
+print("7. Reúne la basura barrida en el recogedor de basura o bolsa de basura:")
+print("    * Utiliza la escoba para empujar la basura hacia el recogedor o bolsa.")
+print("    * Recoge cualquier residuo que se haya quedado atrás.")
+
+print(" ")
+print("8. Desecha la basura adecuadamente:")
+print("    * Lleva el recogedor de basura o bolsa de basura al contenedor correspondiente.")
+print("    * Asegúrate de cerrar bien la bolsa de basura para evitar derrames.")
+
+print(" ")
+print("Finalización:")
+print(" ")
+
+print("9. Guarda la escoba y los materiales de limpieza:")
+print("    * Lava el recogedor de basura o trapo si es necesario.")
+print("    * Guarda la escoba en un lugar adecuado.")
+
+print(" ")
+print("10. ¡Disfruta de tu habitación limpia!")
