f073b14d94a79300579360495b4191b0e6dca87f
mumv1973 <mumv1973@gmail.com>
2024-05-08 18:05:05 -0400

    Juego de mesa

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,29 @@
+
+# Objetivo:
+print("Objetivo: Ser el primer jugador en deshacerte de todas tus cartas.")
+
+# Preparación:
+print("Preparación:")
+print(" 1. Jugadores: De 2 a 8 jugadores.")
+print(" 2. Baraja: Baraja UNO estándar de 108 cartas.")
+print(" 3. Repartir: Baraja las cartas y reparte 7 a cada jugador.Coloca el resto de las cartas boca abajo en el centro como pila de robo.")
+print(" 4. Empezar: El jugador a la izquierda del mazo comienza.")
+
+# Desarrollo del juego:
+print("Desarrollo del juego:")
+print(" 1. En tu turno: Elige una carta de tu mano y colócala boca arriba sobre la pila de descarte. La carta debe coincidir con la carta superior del mazo por color o número.")
+print(" 2. Opciones:")
+print(" Si no tienes una carta coincidente:")
+print(" Roba una carta: Toma una carta de la pila de robo y termina tu turno.")
+print(" Salta un turno: Di \"UNO\" y pasa tu turno al siguiente jugador.")
+print(" Cartas especiales:")
+print(" Salta: El siguiente jugador pierde su turno.")
+print(" Marcha atrás: El orden de juego se invierte.")
+print(" Roba dos: El siguiente jugador roba dos cartas y pierde su turno.")
+print(" Comodín: Elige un color para continuar el juego.")
+print(" 'UNO': Cuando te quede una carta, grita '¡UNO!'. Si no lo haces, tendrás que robar una carta.")
+print(" Fin del juego: El primer jugador en deshacerse de todas sus cartas gana la partida. Los demás jugadores suman los valores de las cartas que les quedan en la mano. El jugador con la menor puntuación final gana el juego.")
+
+# ¡Que comience el juego!
+print(" ")
+print("¡Que comience el juego  🐱‍👤")
