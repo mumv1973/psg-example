35b6735743e049503646b543f6c8798e1fa50a45
mumv1973 <mumv1973@gmail.com>
2024-05-10 14:09:20 -0400

    Ejercicio Total ganado

---
+++ retos_sesion_05/ejercicio_01.py
@@ -0,0 +1,8 @@
+""" Escribe un programa en Python que calcule el salario si la cantidad de horas trabajadas es 160 y la tarifa por hora es 5.5 Usd/hora"""
+
+horas = 160
+pago_hora = 5.5 
+
+total = horas * pago_hora
+
+print("Total ganado: " + str(total))
