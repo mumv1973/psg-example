af7bdd616ff2ac7c354d68fa8514bd49c018c63e
mumv1973 <mumv1973@gmail.com>
2024-05-11 19:52:41 -0400

    Ejercicio 01

---
+++ retos_sesion_06/ejercicio_01.py
@@ -0,0 +1,15 @@
+# ¿Si se declara una variable boolean del valor - 1 es True o False?
+
+print(bool(-15))
+
+print("Es True")
+
+# Construir el operador XNOR en Python 
+
+print(not (True or True))
+print(not (True or False))
+print(not (False or True))
+print(not (False or False))
+
+
+
