c77bc6e7103e33713bac8128c955bf08518a6e84
mumv1973 <mumv1973@gmail.com>
2024-05-20 22:06:12 -0400

    ejercicio 02

---
+++ retos_sesion_07/ejercicio_02.py
@@ -0,0 +1,12 @@
+num1 = input("Ingresa un numero: ")
+num2 = input("Ingresa otro numero: ")
+
+num1 = int(num1)
+num2 = int(num2)
+resultado = num1 + num2
+
+print(num1)
+print(num2)
+
+print(f"El resultado de: {num1} + {num2} es igual a: {resultado}")
+
