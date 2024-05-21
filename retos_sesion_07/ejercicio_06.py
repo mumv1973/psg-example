a757bcb0a065e7e9c55e6ae702b78a4d96785552
mumv1973 <mumv1973@gmail.com>
2024-05-20 22:10:11 -0400

    Ejercicio 06

---
+++ retos_sesion_07/ejercicio_06.py
@@ -0,0 +1,35 @@
+print(" 1  str.removeprefix(prefix, /) If the string starts with the prefix string, return string[len(prefix):]. Otherwise, return a copy of the original string:")
+
+palabra1 = "TestHook"
+print(palabra1)
+nueva_palabra = palabra1.removeprefix("Test")
+print(nueva_palabra)
+
+palabra2 = 'BaseTestCase'
+print(palabra2)
+nueva_palabra2 = palabra2.removeprefix('Test')
+print(nueva_palabra2)
+
+
+print(" 2.-  str.zfill(width) Return a copy of the string left filled with ASCII '0' digits to make a string of length width. A leading sign prefix('+'/'-') is handled by inserting the padding after the sign character rather than before. The original string is returned if width is less than or equal to len(s). For example:")
+
+numero3 = "42"
+print(numero3)
+nuevo_num3 = numero3.zfill(5)
+print(nuevo_num3)
+
+numero4 = "-42"
+nuevo_num4 = numero4.zfill(5)
+print(nuevo_num4)
+
+
+
+
+
+
+print (" 3 ")
+
+print (" 4 ")
+
+print (" 5 ")
+
