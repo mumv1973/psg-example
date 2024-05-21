6e0749338ae2db29d9365953aed9613a7b6de8a1
mumv1973 <mumv1973@gmail.com>
2024-05-20 22:03:52 -0400

    ejercicio 05

---
+++ retos_sesion_06/ejercicio_05.py
@@ -0,0 +1,21 @@
+# Comparar los números 123 y 890, comprobar si tienen la misma paridad ambos pares o ambos impares
+
+
+print(123 % 2)
+
+print("123 es impar")
+print('******************************')
+
+print(890 % 2)
+
+print("890 es par")
+
+print('******************************')
+print((123 % 2) == (890 % 2))
+print('******************************')
+
+print("No, no tienen la misma paridad, el resultado es False")
+
+
+
+
