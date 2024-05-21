12db10a5913720d61da42776509f6b1135849881
mumv1973 <mumv1973@gmail.com>
2024-05-20 22:09:45 -0400

    ejercicio 05

---
+++ retos_sesion_07/ejercicio_05.py
@@ -0,0 +1,9 @@
+verificar = input ('ingresa la palabra o frase *sin espacios* a verificar: ')
+
+print(verificar)
+
+palindromo = verificar[::-1]
+
+print(palindromo)
+
+print(verificar == palindromo)
